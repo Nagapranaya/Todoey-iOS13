399c8c2fae766bda7a700df98248650e296008b6
Nagapranaya Vemula <nagapranaya.vemula@gmail.com>
2024-06-18 10:52:15 +0530

    changes made

---
+++ Todoey/Data Model/item.swift
@@ -0,0 +1,14 @@
+//
+//  item.swift
+//  Todoey
+//
+//  Created by Pranaya on 6/17/24.
+//  Copyright © 2024 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+class Item{
+    var  title: String = ""
+    var done: Bool = false
+}
